40ceec03b2fe3d5dab039ea0fb04f8b52432df72
naveenbudhwal <naveenbudhwal@gmail.com>
2019-07-24 14:21:51 +0530

    Setting Up Backend

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/posts', (req, res) => {
+  res.send([
+    {
+      title: 'Hello World!',
+      description: 'Hi there! How are you?'
+    }
+  ])
+})
+
+app.listen(process.env.PORT || 8082)
